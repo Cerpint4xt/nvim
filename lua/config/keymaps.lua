08c4565ef28b96d410d5d27b4646ff9c6d815371
Raul Catacora <raul.catacora@bupunku.com>
2023-11-24 15:44:39 -0400

    chore: add compiler with leader and opening vimwiki

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,14 @@
 local keymap = vim.keymap
 
 -- Directory Navigation
-keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", {noremap = true, silent = true})
-keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
-
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Pane Navigation
 keymap.set("n", "<C-h>", "<C-w>h", opts)
 keymap.set("n", "<C-j>", "<C-w>j", opts)
 keymap.set("n", "<C-k>", "<C-w>k", opts)
 keymap.set("n", "<C-l>", "<C-w>l", opts)
-
 
 -- Window Management
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
@@ -25,3 +23,11 @@ keymap.set("v", ">", ">gv")
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 
+-- Compile document, be it groff/LaTeX/markdown/etc.
+keymap.set("n", "<leader>c", ':w! | !compiler "%:p"<CR>', opts)
+
+-- Open corresponding .pdf/,html or preview
+keymap.set("n", "<leader>p", ':!opout "%:p"<CR>', opts)
+
+-- Open vimWiki
+keymap.set("n", "<leader>v", ":VimwikiIndex<CR>")
