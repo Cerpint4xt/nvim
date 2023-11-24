52caba14a0396e2e643c3bf261bbd81821bc3064
Raul Catacora <raul.catacora@bupunku.com>
2023-11-24 15:45:26 -0400

    chore: create function and call it in config for vimwiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,8 +1,19 @@
+local config = function()
+	local s = { [".Rmd"] = "markdown", [".rmd"] = "markdown", [".md"] = "markdown", [".mdown"] = "markdown" }
+	local l = {}
+	l.path = "~/.local/share/nvim/vimwiki"
+	l.syntax = "markdown"
+	l.ext = ".md"
+	vim.g.vimwiki_ext2syntax = s
+	vim.g.vimwiki_list = {
+		l,
+	}
+end
+
 return {
-  "vimwiki/vimwiki",
-  event = "VeryLazy",
-  enabled = true,
-  init = function()
-    vim.g.vimwiki_auto_chdir = 1
-  end,
+	"vimwiki/vimwiki",
+	lazy = false,
+	event = "VeryLazy",
+	enabled = true,
+	config = config,
 }
